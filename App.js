0ad22b123d9e8bcf5b1e5bfbd4b12f95ae6b4a6b
Sameed Khan <sameed@healthmatch.io>
2023-01-15 11:29:06 +0600

    with list display

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Searchbar, Card } from 'react-native-paper';
+
+const API_KEY = '183daca270264bad86fc5b72972fb82a'
+
+const fetchNews = (topic) => {
+  return fetch(`https://newsapi.org/v2/everything?q=Apple&from=2023-01-15&sortBy=popularity&apiKey=${API_KEY}`)
+  .then(res => res.json())
+
+}
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [news, setNews] = useState([])
+  const [loading, isLoading] = useState(true)
+  useEffect(() => {
+    fetchNews()
+    .then(data => setNews(data))
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Searchbar
+        placeholder="Search"
+        onChangeText={query => setSearchQuery(query)}
+        value={searchQuery}
+        style={styles.searchBar}
+      />
+      <FlatList
+        data={news?.articles || []}
+        renderItem={({item}) => {
+          return (
+            <View  style={styles.newsItem}>
+              <Card>
+                <Card.Title title={item.title} subtitle={item.description}/>
+                <Card.Content>
+                  <Text variant="titleLarge">{item.content}</Text>
+                </Card.Content>
+                <Card.Cover source={{ uri: item.urlToImage }} />
+              </Card>
+            </View>
+          )
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 5,
+    paddingTop: 25,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  newsItem: {
+    marginVertical: 5
+  },
+  searchBar: {
+    marginBottom: 10
+  }
 });
